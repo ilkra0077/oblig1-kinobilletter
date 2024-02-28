e68205417723288424030d1db3a5e4a847f0f4ec
ilkra0077 <ilkra0077@oslomet.no>
2024-02-28 20:42:08 +0100

    - validering av email og tlf

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,31 +1,24 @@
 const billettRegister = [];
 
 function visBillettRegister() {
-    // skriv ut
+    // Skriv ut
     let ut = "<table><tr>" +
         "<th>Film</th><th>Antall</th><th>Fornavn</th><th>Etternavn</th><th>Telefonnr</th><th>Email</th>" +
         "</tr>";
+
     for (let p of billettRegister) {
         ut += "<tr>";
         ut += "<td>" + p.film + "</td><td>" + p.antall + "</td><td>" + p.fornavn + "</td><td>" + p.etternavn + "</td><td>" + p.telefonnr + "</td><td>" + p.email + "</td>";
         ut += "</tr>";
     }
+
     $("#billettRegister").html(ut);
 }
 
-function validerEmail(email) {
-    // sjekker om email har @
-    return email.includes('@');
-}
-
-function validerTelefonnr(telefonnr) {
-    // sjekker om nummer har bare nummer, og 8 tegn
-    return /^\d{8}$/.test(telefonnr);
-}
-
 function kjopBilletter() {
-    // Fjerner error meldinger før neste kjøp av billett
+    // Fjerner error-meldinger før neste kjøp av billett
     $('span.error-message').remove();
+    $("#error-container").html("");
 
     const film = $("#film").val();
     const antall = $("#antall").val();
@@ -34,29 +27,31 @@ function kjopBilletter() {
     const telefonnr = $("#telefonnr").val();
     const email = $("#email").val();
 
-    if (film === '' || antall === '' || fornavn === '' || etternavn === '' || telefonnr === '' || email === '') {
-        const errorMelding = '<span class="error-message" style="color: red;">Du må fylle inn felt</span>';
+    const emailValidering = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const erEmailValid = emailValidering.test(email);
+
+    const telefonnrValidering = /^\+?(\d{2,4}\s?[\s.-]?)?\d{2,12}$/;
+    const erTelefonnrValid = telefonnrValidering.test(telefonnr);
+
+    if (film === '' || antall === '' || fornavn === '' || etternavn === '' || telefonnr === '' || !erTelefonnrValid || email === '' || !erEmailValid) {
+        const errorMelding = '<span class="error-message" style="color:red;">Ugyldig. Fyll inn riktig data.</span>';
         if (film === '') {
             $('#film').after(errorMelding);
         }
         if (antall === '') {
-            $('#antall').after(errorMelding)
+            $('#antall').after(errorMelding);
         }
         if (fornavn === '') {
-            $('#fornavn').after(errorMelding)
+            $('#fornavn').after(errorMelding);
         }
         if (etternavn === '') {
-            $('#etternavn').after(errorMelding)
+            $('#etternavn').after(errorMelding);
         }
-        if (telefonnr === '') {
-            $('#telefonnr').after(errorMelding)
-        } else if (!validerTelefonnr(telefonnr)) {
-            $('#telefonnr').after('<span class="error-message" style="color: red;"> Putt inn et korrekt nummer</span>');
+        if (telefonnr === '' ||!erTelefonnrValid) {
+            $('#telefonnr').after(errorMelding);
         }
-        if (email === '') {
-            $('#email').after(errorMelding)
-        } else if (!validerEmail(email)) {
-            $('#email').after('<span class="error-message" style="color: red;">Putt inn korrekt email</span>');
+        if (email === '' || !erEmailValid) {
+            $('#email').after(errorMelding);
         }
     } else {
         const billett = {
@@ -80,5 +75,3 @@ function slettBilletter() {
     billettRegister.length = 0;
     visBillettRegister();
 }
-
-
